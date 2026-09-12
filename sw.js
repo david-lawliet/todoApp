6d1920871f37f87906e3bb439352691953f130f4
thaonx <thaonx@intern.vn>
2026-09-12 09:48:34 +0700

    Update app logo to user provided image, add apple-touch-icon for iOS

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'lifetracker-v12';
+const CACHE_NAME = 'lifetracker-v13';
 const urlsToCache = [
   './',
   './index.html',
   './style.css',
   './app.js',
   './manifest.json',
-  './icon.svg',
+  './app-logo.png',
   './universfield-alarm-clock-ringing-229175.mp3',
   './universfield-digital-alarm-clock-02-151919.mp3'
 ];
