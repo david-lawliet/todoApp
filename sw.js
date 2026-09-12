7247dfb4fd21836415607e5c90926e7e05d1e3eb
thaonx <thaonx@intern.vn>
2026-09-12 08:07:34 +0700

    Update SW for immediate claim, force pill shape on tabs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lifetracker-v3';
+const CACHE_NAME = 'lifetracker-v4';
 const urlsToCache = [
   './',
   './index.html',
@@ -9,6 +9,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
@@ -46,6 +47,7 @@ self.addEventListener('fetch', event => {
 });
 
 self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
